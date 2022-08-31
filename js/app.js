6582a938306f9323a63b056472ab08b53a5e11cc
Emilse Arias <emilsearias@gmail.com>
2022-08-31 10:37:06 -0300

    agregamos el redireccionamiento cuando seleccionamos la card de cada pelicula

--- js/app.js
+++ js/app.js
@@ -30,4 +30,7 @@ padre.innerHTML += `
 
 function verDetalle(codigo){
     console.log(codigo);
+    console.log(window.location);
+    console.log(window.location.origin+'/pages/detalle.html?codigo='+codigo);
+    window.location.href = window.location.origin+'/pages/detalle.html?codigo='+codigo
 }
